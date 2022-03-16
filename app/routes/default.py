81b337090228828fd42ae1cc9cd168c6979dd9c6
leahmarquezz <s_leah.marquez@ousd.org>
2022-03-16 09:51:28 -0700

    first commit

--- app/routes/default.py
+++ app/routes/default.py
@@ -2,10 +2,27 @@ from app import app
 from flask import render_template
 
 # This is for rendering the home page
+@app.route('/index')
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/test')
-def test():
-    return render_template('test.html')
+@app.route('/aboutus')
+def aboutus():
+    return render_template('aboutus.html')
+
+@app.route('/challenge')
+def challenge():
+    return render_template('challenge.html')
+    
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/sharehome')
+def sharehome():
+    return render_template('sharehome.html')
+
+@app.route('/userhome')
+def userhome():
+    return render_template('userhome.html')
